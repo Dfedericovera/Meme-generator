0efadd1a32e7332c542251d679355e53e3fa9bf2
Federico Vera <dfedericovera@gmail.com>
2023-04-21 22:21:23 -0300

    Se agregan imagenes aleatoriamente usando useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from "react"
 import Header from "./components/Header"
+import Meme from "./components/Meme"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <Header />
+    <Meme />
     </>
   )
 }
